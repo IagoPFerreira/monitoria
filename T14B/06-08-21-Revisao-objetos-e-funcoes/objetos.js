ad8f6a0d659d3047b1101fcd34ddbee0c4847af6
iago <iago.pferreiravr@gmail.com>
2021-08-06 16:04:51 -0300

    Commitao BOMBA

---
+++ T14B/06-08-21-Revisao-objetos-e-funcoes/objetos.js
@@ -0,0 +1,177 @@
+// composta: array, string, 
+// {} 
+// simples: number, string, boolean
+
+// Um objeto é:
+// Um conjunto de dados armazenados no formato chave: valor 
+
+// const string =  'Olá, mundo!';
+
+// const number = 2;
+
+// const array = ['string', null, undefined, symbol, NaN, 65, true];
+
+// const objeto = {
+//   string: 'string',
+//   null,
+//   undefined,
+//   symbol: symbol,
+//   nan: NaN,
+//   number: 65,
+//   bool: true
+// }
+
+// console.log(typeof NaN);
+
+// Number.NaN()
+
+// console.log(array[bool]);
+
+// console.log(string[2]);
+
+// const string =  'Olá, mundo!';
+
+// const number = 2;
+
+// const string = {
+//   length,
+//   toUpperCase,
+//   toLowerCase,
+//   trim,
+// }
+
+// string.
+
+// const casa = {
+//   quarto: {
+//     cama: {
+//       travesseiro: 'Fofinho'
+//     }
+//   },
+//   sala: {
+//     sofa: {
+//       almofada: 'top de linha',
+//       encosto: 'macio'
+//     }
+//   },
+//   cozinha: {
+//     geladeira: 'cheia',
+//     fogão: {
+//       acender: () => console.log('E fez-se o fogo!'),
+//       apagar: '',
+//       cozinhar: {}
+//     }
+//   },
+//   banheiro: '',
+// }
+
+// const array = ['string', null, undefined, 'symbol', 'NaN', 65, true];
+
+// console.log(array.map((item) => item));
+
+// console.log(casa.quarto.cama.travesseiro);
+
+// const { travesseiro } = casa.quarto.cama;
+
+// const {
+//   quarto: { cama: { travesseiro }, cama },
+//   sala: { sofa: { encosto, almofada } },
+//   cozinha: {
+//     geladeira, 
+//     fogão: { acender }
+//   },
+//   sala,
+//   cozinha
+// } = casa
+
+// const { travesseiro, encosto, fogao} = casa
+
+
+// console.log(sala);
+// cozinha.fogão.acender()
+// acender();
+
+// console.log(sala);
+// console.log(cama);
+
+// console.log(almofada);
+// console.log(encosto);
+// console.log(travesseiro);
+
+// console.log(casa['quarto']['cama']['travesseiro']);
+
+// const corpo = {
+//   cabeca: {
+//     cranio,
+//     cerebro,
+//     musculos,
+//     pele,
+//     tecidoNervoso
+//   },
+//   tronco,
+//   membrosSuperiores,
+//   membrosInferiores,
+// }
+
+// const a = [];
+
+// a[1] = 1;
+// a[2] = 2;
+
+// console.log(a.length); 
+
+// const objeto = { string: "String", 1: 24, 2: undefined} 
+// const objeto2 = ["String", 24, undefined]
+
+// console.log(objeto.string);
+// console.log(objeto2[objeto2.length - 2]);
+
+function tarara(param1, param2) {
+  console.log(param1);
+  const frase = `${param1} tem ${param2} anos`
+
+  return frase
+}
+
+// function () {}
+
+// () => {}
+
+const xablau = () => {
+  const argumento1 = 'Elmo';
+  const argumento2 = 100;
+
+  const elmoFrase = tarara(argumento1, argumento2);
+  return elmoFrase;
+}
+
+console.log(xablau());
+
+console.log(tarara('Hugo', 36));
+
+// const tururu = () => ({
+//   quarto: {
+//         cama: {
+//           travesseiro: 'Fofinho'
+//         }
+//       },
+//       sala: {
+//         sofa: {
+//           almofada: 'top de linha',
+//           encosto: 'macio'
+//         }
+//       },
+//       cozinha: {
+//         geladeira: 'cheia',
+//         fogão: {
+//           acender: () => console.log('E fez-se o fogo!'),
+//           apagar: '',
+//           cozinhar: {}
+//         }
+//       },
+//       banheiro: '',
+// });
+
+// console.log(tururu());
+// console.log(tarara(654, 45));
+
