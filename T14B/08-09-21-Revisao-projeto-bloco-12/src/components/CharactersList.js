8e263d70646d8713bd2ea49ecd79df7b219f1a53
iago <iago.pferreiravr@gmail.com>
2021-09-08 11:41:00 -0300

    Implementacao das funcoes results & renderHeroes

--- T14B/08-09-21-Revisao-projeto-bloco-12/src/components/CharactersList.js
+++ T14B/08-09-21-Revisao-projeto-bloco-12/src/components/CharactersList.js
@@ -1,11 +1,50 @@
 import React from 'react';
+import fetcher from '../services/fetch';
+import CharacterCard from './CharacterCard';
 
-const CharactersList = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+class CharactersList extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      isLoading: true,
+      charactersList: ''
+    }
+  };
+
+  results = () => {
+    const { charactersList } = this.state;
+
+    if (charactersList) return true;
+
+    fetcher.then((result) => {
+      this.setState({
+        isLoading: false,
+        charactersList: result
+      })
+    });
+  };
+  
+  renderHeroes = () => {
+    const { charactersList } = this.state;
+    return charactersList.map((character) => (
+      <CharacterCard key={character.id} character={ character } />
+    ))
+  };
+
+  render() {
+    this.results();
+    const { isLoading } = this.state;
+    // console.log('List');
+    // console.log(charactersList);
+    return (
+      <section>
+        { isLoading ? <p>Loading</p> : this.renderHeroes() }
+      </section>
+
+      // <>
+    );
+  }
 };
 
 export default CharactersList;
