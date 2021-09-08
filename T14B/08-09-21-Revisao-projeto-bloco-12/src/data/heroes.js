7725a977d7d9c269146cb8159c2ab72a51d5bf05
iago <iago.pferreiravr@gmail.com>
2021-09-08 09:08:44 -0300

    Criando a base de dados do projeto

---
+++ T14B/08-09-21-Revisao-projeto-bloco-12/src/data/heroes.js
@@ -0,0 +1,616 @@
+const heroes = [
+  {
+    "id": "162",
+    "name": "Carnage",
+    "powerstats": {
+      "intelligence": "63",
+      "strength": "63",
+      "speed": "70",
+      "durability": "84",
+      "power": "88",
+      "combat": "90"
+    },
+    "biography": {
+      "full-name": "Cletus Kasady",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "Mass Carnage",
+        "999th"
+      ],
+      "place-of-birth": "-",
+      "first-appearance": "Amazing Spider-Man #344 (February, 1991)",
+      "publisher": "Marvel Comics",
+      "alignment": "bad"
+    },
+    "appearance": {
+      "gender": "Male",
+      "race": "Symbiote",
+      "height": [
+        "6'1",
+        "185 cm"
+      ],
+      "weight": [
+        "190 lb",
+        "86 kg"
+      ],
+      "eye-color": "Green",
+      "hair-color": "Red"
+    },
+    "work": {
+      "occupation": "-",
+      "base": "New York City"
+    },
+    "connections": {
+      "group-affiliation": "Maximum Carnage: (Carrion, Shriek, Demogoblin, Doppelganger)",
+      "relatives": "Venom (\"father\"); Shriek (\"wife\"); Hybrid, Lasher, Phage, Riot (\"brothers\"); Agony, Scream (\"sisters\"); Carrion, Demogoblin, Doppelganger, Toxin (\"sons\"); Scorn (\"Daughter\")"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/187.jpg"
+    }
+  },
+  {
+    "id": "572",
+    "name": "Sandman",
+    "powerstats": {
+      "intelligence": "44",
+      "strength": "75",
+      "speed": "46",
+      "durability": "90",
+      "power": "100",
+      "combat": "60"
+    },
+    "biography": {
+      "full-name": "William Baker",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "Flint Marko",
+        "Sylvester Mann",
+        "Quarryman"
+      ],
+      "place-of-birth": "Queens, New York",
+      "first-appearance": "Amazing Spider-Man #4 (September, 1963)",
+      "publisher": "Marvel Comics",
+      "alignment": "neutral"
+    },
+    "appearance": {
+      "gender": "Male",
+      "race": "Human",
+      "height": [
+        "6'1",
+        "185 cm"
+      ],
+      "weight": [
+        "450 lb",
+        "203 kg"
+      ],
+      "eye-color": "Brown",
+      "hair-color": "Brown"
+    },
+    "work": {
+      "occupation": "Professional Criminal",
+      "base": "-"
+    },
+    "connections": {
+      "group-affiliation": "(Formerly) Sinister Twelve, the Sinister Six, the Frightful Four, the Outlaws and the Wild Pack, reserve member of the Avengers",
+      "relatives": "Mrs. Baker (mother), unidentified cousin"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/621.jpg"
+    }
+  },
+  {
+    "id": "699",
+    "name": "Vixen",
+    "powerstats": {
+      "intelligence": "50",
+      "strength": "38",
+      "speed": "50",
+      "durability": "50",
+      "power": "62",
+      "combat": "25"
+    },
+    "biography": {
+      "full-name": "Mari McCabe",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "-"
+      ],
+      "place-of-birth": "Zambesi",
+      "first-appearance": "Action Comics #521 (July, 1981)",
+      "publisher": "DC Comics",
+      "alignment": "good"
+    },
+    "appearance": {
+      "gender": "Female",
+      "race": "Human",
+      "height": [
+        "5'9",
+        "175 cm"
+      ],
+      "weight": [
+        "140 lb",
+        "63 kg"
+      ],
+      "eye-color": "Amber",
+      "hair-color": "Black"
+    },
+    "work": {
+      "occupation": "Former Model",
+      "base": "-"
+    },
+    "connections": {
+      "group-affiliation": "Formerly Suicide Squad, Checkmate, Ultramarines, Birds of Prey, Justice League of America",
+      "relatives": "Reverend Richard Jiwe (father, deceased), Jeanne-Mari Jiwe (mother, deceased), General Mustapha Maksai (uncle, deceased), Tantu (ancestor)"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/10109.jpg"
+    }
+  },
+  {
+    "id": "723",
+    "name": "X-23",
+    "powerstats": {
+      "intelligence": "75",
+      "strength": "24",
+      "speed": "42",
+      "durability": "100",
+      "power": "55",
+      "combat": "95"
+    },
+    "biography": {
+      "full-name": "Laura Kinney",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "Laura Logan",
+        "Laura Howlett",
+        "Laura X",
+        "Talon",
+        "Wolverine",
+        "X23"
+      ],
+      "place-of-birth": "The Facility, location unrevealed",
+      "first-appearance": "NYX #3",
+      "publisher": "Marvel Comics",
+      "alignment": "good"
+    },
+    "appearance": {
+      "gender": "Female",
+      "race": "Mutant / Clone",
+      "height": [
+        "5'1",
+        "155 cm"
+      ],
+      "weight": [
+        "110 lb",
+        "50 kg"
+      ],
+      "eye-color": "Green",
+      "hair-color": "Black"
+    },
+    "work": {
+      "occupation": "Adventurer, Student, former; Assassin, Prostitute",
+      "base": "Mobile. Formerly Avengers Mansion, New York City and Avengers Compound"
+    },
+    "connections": {
+      "group-affiliation": "New X-Men, formerly Xavier Institute Student Body, The Facility",
+      "relatives": "Sarah Kinney (surrogate mother, deceased), James Howlett (Wolverine, genetic progenitor)"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/565.jpg"
+    }
+  },
+  {
+    "id": "470",
+    "name": "Moon Knight",
+    "powerstats": {
+      "intelligence": "50",
+      "strength": "36",
+      "speed": "23",
+      "durability": "42",
+      "power": "28",
+      "combat": "75"
+    },
+    "biography": {
+      "full-name": "Marc Spector",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "Jake Lockley",
+        "Steven Grant",
+        "Yitzak Topol",
+        "Fist of Khonshu",
+        "Crescent Crusader",
+        "Lunar Legionnaire"
+      ],
+      "place-of-birth": "Chicago, Illinois",
+      "first-appearance": "Werewolf by Night #32 (August, 1975)",
+      "publisher": "Marvel Comics",
+      "alignment": "good"
+    },
+    "appearance": {
+      "gender": "Male",
+      "race": "Human",
+      "height": [
+        "6'2",
+        "188 cm"
+      ],
+      "weight": [
+        "225 lb",
+        "101 kg"
+      ],
+      "eye-color": "Brown",
+      "hair-color": "Brown"
+    },
+    "work": {
+      "occupation": "Adventurer, entrepreneur; former prize fighter, spy, soldier, mercenary, cab driver",
+      "base": "New York City; formerly Spector Mansion, Long Island, Avengers Compound, California"
+    },
+    "connections": {
+      "group-affiliation": "Secret Avengers; formerly: US Marine Corps, Central Intelligence Agency (CIA), partner of Jean-Paul du Champ, partner of Raoul Bushman, partner of Midnight, West Coast Avengers, Marvel Knights, Defenders",
+      "relatives": "Shadowknight (Randall Spector) (brother, deceased), Elias (father, deceased), Seth Phalkon (alleged great-great-grandfather)"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/415.jpg"
+    }
+  },
+  {
+    "id": "416",
+    "name": "Luke Cage",
+    "powerstats": {
+      "intelligence": "50",
+      "strength": "63",
+      "speed": "29",
+      "durability": "95",
+      "power": "44",
+      "combat": "70"
+    },
+    "biography": {
+      "full-name": "Luke Cage",
+      "alter-egos": "Power Man",
+      "aliases": [
+        "Carl Lucas",
+        "Power Man",
+        "Mark Lucas",
+        "Hero for Hire",
+        "Ace of Spades"
+      ],
+      "place-of-birth": "Harlem, New York City",
+      "first-appearance": "Hero for Hire #1 (1972)",
+      "publisher": "Power Man",
+      "alignment": "good"
+    },
+    "appearance": {
+      "gender": "Male",
+      "race": "Human",
+      "height": [
+        "6'6",
+        "198 cm"
+      ],
+      "weight": [
+        "425 lb",
+        "191 kg"
+      ],
+      "eye-color": "Brown",
+      "hair-color": "Black"
+    },
+    "work": {
+      "occupation": "Adventurer, bar owner, bodyguard, mercenary; formerly theater owner, private investigator, various odd jobs",
+      "base": "-"
+    },
+    "connections": {
+      "group-affiliation": "Underground Avengers (formerly Daredevil's Unnamed Super-Hero Team), Secret Defenders, Heroes for Hire, Nightwing Restorations, Fantastic Four, Defenders, Bloods",
+      "relatives": "Jessica Jones (wife), James Leonard Lucas (father), Esther (mother, deceased), James Jr. (Coldfire, brother), unnamed daughter"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/269.jpg"
+    }
+  },
+  {
+    "id": "106",
+    "name": "Black Panther",
+    "powerstats": {
+      "intelligence": "88",
+      "strength": "16",
+      "speed": "30",
+      "durability": "60",
+      "power": "41",
+      "combat": "100"
+    },
+    "biography": {
+      "full-name": "T'Challa",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "Mr. Okonkwo",
+        "The Man Without Fear",
+        "Luke Charles",
+        "Black Leopard",
+        "the Client",
+        "Coal Tiger",
+        "has impersonated Daredevil and others on occasion"
+      ],
+      "place-of-birth": "Wakanda, Africa",
+      "first-appearance": "Fantastic Four Vol. 1 #52 (1966)",
+      "publisher": "Marvel Comics",
+      "alignment": "good"
+    },
+    "appearance": {
+      "gender": "Male",
+      "race": "Human",
+      "height": [
+        "6'0",
+        "183 cm"
+      ],
+      "weight": [
+        "200 lb",
+        "90 kg"
+      ],
+      "eye-color": "Brown",
+      "hair-color": "Black"
+    },
+    "work": {
+      "occupation": "King and Chieftain of Wakanda, scientist; former school teacher",
+      "base": "Wakanda, Mobile"
+    },
+    "connections": {
+      "group-affiliation": "Formerly Fantastic Four, Secret Avengers, Avengers, Pendragons, Queen's Vengeance, former Fantastic Force financier",
+      "relatives": "Bashenga (paternal ancestor, deceased), Azzuri the Wise (paternal grandfather, deceased), Nanali (paternal grandmother, deceased), Chanda (paternal grandfather, presumably deceased), T�Chaka (father, deceased), S'Yan (uncle, deceased), N�Yami (mother, deceased), Ramonda (stepmother), Hunter (adopted brother), Jakarra (half-brother), Shuri (sister), Ororo Munroe (wife), Joshua Itobo, Ishanta, Zuni, M'Koni, T'Shan (cousins), Wheeler (cousin by marriage, deceased), Billy Wheeler (1st cousin once removed)"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/247.jpg"
+    }
+  },
+  {
+    "id": "708",
+    "name": "Wasp",
+    "powerstats": {
+      "intelligence": "63",
+      "strength": "17",
+      "speed": "58",
+      "durability": "52",
+      "power": "29",
+      "combat": "42"
+    },
+    "biography": {
+      "full-name": "Janet Van Dyne",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "Pixie"
+      ],
+      "place-of-birth": "Cresskill, New Jersey",
+      "first-appearance": "TALES TO ASTONISH #44",
+      "publisher": "Marvel Comics",
+      "alignment": "good"
+    },
+    "appearance": {
+      "gender": "Female",
+      "race": "Human",
+      "height": [
+        "5'4",
+        "163 cm"
+      ],
+      "weight": [
+        "110 lb",
+        "50 kg"
+      ],
+      "eye-color": "Blue",
+      "hair-color": "Auburn"
+    },
+    "work": {
+      "occupation": "Adventurer, fashion designer, independently wealthy socialite",
+      "base": "Avengers Mansion"
+    },
+    "connections": {
+      "group-affiliation": "Avengers",
+      "relatives": "Vernon Van Dyne (father, deceased), unnamed mother (deceased), Henry \"Hank\" Pym (ex-husband)"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/155.jpg"
+    }
+  },
+  {
+    "id": "413",
+    "name": "Lobo",
+    "powerstats": {
+      "intelligence": "94",
+      "strength": "100",
+      "speed": "54",
+      "durability": "100",
+      "power": "100",
+      "combat": "85"
+    },
+    "biography": {
+      "full-name": "Lobo",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "The Main Man",
+        "The 'Bo",
+        "Master Frag",
+        "Mister Machete",
+        "Popebo; He-Who-Devours-Your-Entrails-And-Thoroughly-Enjoys-It"
+      ],
+      "place-of-birth": "-",
+      "first-appearance": "Omega Men # 3",
+      "publisher": "DC Comics",
+      "alignment": "neutral"
+    },
+    "appearance": {
+      "gender": "Male",
+      "race": "Czarnian",
+      "height": [
+        "7'6",
+        "229 cm"
+      ],
+      "weight": [
+        "640 lb",
+        "288 kg"
+      ],
+      "eye-color": "Red",
+      "hair-color": "Black"
+    },
+    "work": {
+      "occupation": "Assassin, Bounty Hunter, Priest",
+      "base": "Mobile throughout several galaxies"
+    },
+    "connections": {
+      "group-affiliation": "Formerly LEGION, Young Justice; First Celestial Church of the Triple Fish-God",
+      "relatives": "Slobo (clone)"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/1127.jpg"
+    }
+  },
+  {
+    "id": "208",
+    "name": "Darth Vader",
+    "powerstats": {
+      "intelligence": "69",
+      "strength": "48",
+      "speed": "33",
+      "durability": "35",
+      "power": "100",
+      "combat": "100"
+    },
+    "biography": {
+      "full-name": "Anakin Skywalker",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "Lord Vader"
+      ],
+      "place-of-birth": "Tatooine",
+      "first-appearance": "Star Wars: Episode IV - A New Hope (1977)",
+      "publisher": "George Lucas",
+      "alignment": "bad"
+    },
+    "appearance": {
+      "gender": "Male",
+      "race": "Cyborg",
+      "height": [
+        "6'6",
+        "198 cm"
+      ],
+      "weight": [
+        "300 lb",
+        "135 kg"
+      ],
+      "eye-color": "Yellow",
+      "hair-color": "No Hair"
+    },
+    "work": {
+      "occupation": "Sith Lord, Supreme Commander of the Imperial Fleet",
+      "base": "Death Star"
+    },
+    "connections": {
+      "group-affiliation": "Sith, Galactic Empire",
+      "relatives": "Luke Skywalker (Son), Princess Leia (Daughter)"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/10444.jpg"
+    }
+  },
+  {
+    "id": "207",
+    "name": "Darth Maul",
+    "powerstats": {
+      "intelligence": "56",
+      "strength": "48",
+      "speed": "50",
+      "durability": "30",
+      "power": "100",
+      "combat": "100"
+    },
+    "biography": {
+      "full-name": "",
+      "alter-egos": "No alter egos found.",
+      "aliases": [
+        "Sith",
+        "Master Of The Double-Bladed Lightsaber"
+      ],
+      "place-of-birth": "Dathomir",
+      "first-appearance": "Star Wars Episode I: The Phantom Menace (1999)",
+      "publisher": "George Lucas",
+      "alignment": "bad"
+    },
+    "appearance": {
+      "gender": "Male",
+      "race": "Dathomirian Zabrak",
+      "height": [
+        "5'7",
+        "170 cm"
+      ],
+      "weight": [
+        "- lb",
+        "0 kg"
+      ],
+      "eye-color": "Yellow / Red",
+      "hair-color": "-"
+    },
+    "work": {
+      "occupation": "-",
+      "base": "-"
+    },
+    "connections": {
+      "group-affiliation": "-",
+      "relatives": "Darth Sidious (Master)"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/10464.jpg"
+    }
+  },
+  {
+    "id": "213",
+    "name": "Deadpool",
+    "powerstats": {
+      "intelligence": "69",
+      "strength": "32",
+      "speed": "50",
+      "durability": "100",
+      "power": "100",
+      "combat": "100"
+    },
+    "biography": {
+      "full-name": "Wade Wilson",
+      "alter-egos": "Evil Deadpool, Venompool",
+      "aliases": [
+        "Wade Wilson",
+        "Jack",
+        "Chiyonosake (\"the Wolf of the Rice Wine\")",
+        "Rhodes",
+        "Corpus",
+        "Lopez",
+        "Hobgoblin",
+        "Thom Cruz",
+        "Peter Parker"
+      ],
+      "place-of-birth": "Canada",
+      "first-appearance": "New Mutants #98 (February, 1991)",
+      "publisher": "Evil Deadpool",
+      "alignment": "neutral"
+    },
+    "appearance": {
+      "gender": "Male",
+      "race": "Mutant",
+      "height": [
+        "6'2",
+        "188 cm"
+      ],
+      "weight": [
+        "210 lb",
+        "95 kg"
+      ],
+      "eye-color": "Brown",
+      "hair-color": "No Hair"
+    },
+    "work": {
+      "occupation": "Mercenary; former enforcer, government operative, sumo wrestler, soldier, assassin, anti-hero, others",
+      "base": "Cavern-X, Sedona, Arizona, Mobile"
+    },
+    "connections": {
+      "group-affiliation": "Thunderbolts (Strike Team), shares body with Agent Preston; formerly X-Force, Deadpool Corps, Agency X, S.H.I.E.L.D.; Code Red, Six Pack, One World Church, DP Inc., Weapon X, Weapon Plus, Heroes for Hire, Secret Defenders, Frightful Four, Team Deadpool, L",
+      "relatives": "Thomas \"Mickey\" Wilson (father, deceased); Hailey Wilson (mother, deceased); Gretchen Wilson, Orksa (ex-wives); Evil Deadpool (clone); Widdle Wade (clone)"
+    },
+    "image": {
+      "url": "https://www.superherodb.com/pictures2/portraits/10/100/835.jpg"
+    }
+  },
+]
+
+export default heroes;
